33d2dcc138b57ba0513dd5465031833a3eb61360
esthercordova <estherpiacordova@gmail.com>
2016-06-30 11:27:51 -0400

    Add api.js, app.js and events.js for ajax requests

---
+++ assets/scripts/api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const app = require('../app.js');
+
+const getBooks = () => {
+  return $.ajax({
+    url: app.host + '/books/',
+    method: "GET",
+  });
+};
+
+
+module.exports = {
+  getBooks,
+};
