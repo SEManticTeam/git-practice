0606bca7da6f6a0b694495bc1f7d2251dae46cb3
SarahHartwick <sarah@surfsetfitness.com>
2016-06-30 11:42:24 -0400

    Added handlers on document load

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,14 @@
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+// let example = require('./example');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+const events = require('./events.js');
+
+
+
+
+// On document ready
+$(() => {
+  events.addHandlers();
+});
